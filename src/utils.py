715ab4d6d42503e6eb2d5df8df21fc2065caf053
Jekaterina Macilevic <jekaterinam@jekaterinas-mbp.home>
2024-08-14 11:49:06 +0100

    added new function for fetching URL data in utils.py to match the unittest class TestFetchUrlData

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,33 @@
 from urllib.parse import urlparse
+import requests
+from time import time
 
+# Checking if the URL is valid
 def validate_url(url):
     if not isinstance(url, str):
         return False
     
     result = urlparse(url)
-    print(f"Parsing URL: {url}")
-    print(f"Scheme: {result.scheme}")
-    print(f"Netloc: {result.netloc}")
-
-    # Ensure that scheme is either 'http' or 'https'
     valid_scheme = result.scheme in ("http", "https")
-    
-    # Ensure that netloc is not empty and contains a valid hostname
     valid_netloc = bool(result.netloc) and not result.netloc.startswith(':') and ':' not in result.netloc
 
     return valid_scheme and valid_netloc
+
+## Fetching URL data
+def fetch_url_data(url):
+    start_time = time()
+    try:
+        response = requests.get(url, timeout=5)
+        duration = int((time() - start_time) * 1000) 
+        return {
+            "url": url,
+            "statusCode": response.status_code,
+            "contentLength": int(response.headers.get('Content-Length', 0)),
+            "requestDuration": f"{duration}ms",
+            "date": response.headers.get('Date', "No date")
+        }
+    except requests.exceptions.RequestException as e:
+        return {
+            "url": url,
+            "error": str(e)
+        }
